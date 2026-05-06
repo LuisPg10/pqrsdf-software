a78ad8e5dd7d0d3175fd3412e0fe771e27c9a991
Luis Pinto <dapin46@hotmail.com>
2026-05-06 08:09:58 -0500

    update: cancelation token provides in login and register

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -10,9 +10,9 @@ public class AuthController(ISender mediator) : ApiController
   [ProducesResponseType(StatusCodes.Status200OK)]
   [ProducesResponseType(StatusCodes.Status404NotFound)]
   [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-  public async Task<IActionResult> Login([FromBody] LoginQueryDto request)
+  public async Task<IActionResult> Login([FromBody] LoginQueryDto request, CancellationToken cancellationToken)
   {
-    var result = await mediator.Send(request, CancellationToken.None);
+    var result = await mediator.Send(request, cancellationToken);
 
     return result.Match(
       Ok,
@@ -24,9 +24,9 @@ public class AuthController(ISender mediator) : ApiController
   [ProducesResponseType(StatusCodes.Status201Created)]
   [ProducesResponseType(StatusCodes.Status400BadRequest)]
   [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-  public async Task<IActionResult> Register([FromBody] RegisterCommandDto request)
+  public async Task<IActionResult> Register([FromBody] RegisterCommandDto request, CancellationToken cancellationToken)
   {
-    var result = await mediator.Send(request, CancellationToken.None);
+    var result = await mediator.Send(request, cancellationToken);
 
     return result.Match(
       Ok,
